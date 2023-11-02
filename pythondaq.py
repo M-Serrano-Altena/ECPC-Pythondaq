b655f50624b5615d53fc57bcd67c4eba47940230
M-Serrano-Altena <Marc.Serrano.Altena@gmail.com>
2023-11-02 10:16:51 +0100

    added what channels measure what current

--- pythondaq.py
+++ pythondaq.py
@@ -10,6 +10,10 @@ device = rm.open_resource(
     "ASRL5::INSTR", read_termination="\r\n", write_termination="\n"
 )
 
+# U0 has the full current
+# U1 measures the current passing through the LED and resistor, which is the full current
+# U2 measures the current passing through the resistor
+
 # voltage increases till the max intensity
 def light():
     # 1023 is de max output, 1024 geeft een spanning van 0
@@ -17,3 +21,4 @@ def light():
     return
 
 light()
+device.query(f"OUT:CH0 {0}")
