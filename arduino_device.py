8a45e04680890ba09b21fb54bdb1c3559a93a66b
M-Serrano-Altena <Marc.Serrano.Altena@gmail.com>
2023-11-06 15:12:12 +0100

    Split the controller

---
+++ arduino_device.py
@@ -0,0 +1,40 @@
+import pyvisa
+
+def list_devices():
+    rm = pyvisa.ResourceManager("@py")
+
+    return rm.list_resources()
+
+class ArduinoVISADevice:
+
+    def __init__(self, port):
+        self.port = port
+        self.rm = pyvisa.ResourceManager("@py")
+        self.device = self.rm.open_resource(
+            "ASRL5::INSTR", read_termination="\r\n", write_termination="\n"
+        )
+
+    @staticmethod
+    def ADC_to_volt(ADC):
+        return round(int(ADC) * 3.3/1023, 2)
+
+    @staticmethod
+    def volt_to_ADC(U):
+        return int(round(U * 1023/3.3))
+
+    def get_identification(self):
+        return self.device.query("*IDN?")
+
+    def set_output_value(self, adc_value):
+        # adc_value % 1024 to get values between 0 and 1023
+        self.device.query(f"OUT:CH0 {adc_value % 1024}")
+        return
+
+    def get_output_value(self):
+        return self.device.query("OUT:CH0?")
+
+    def get_input_value(self, channel):
+        return int(self.device.query(f"MEAS:CH{channel}?"))
+    
+    def get_input_value_voltage(self, channel):
+        return self.ADC_to_volt(self.device.query(f"MEAS:CH{channel}?"))
--- pythondaq.py
+++ pythondaq.py
@@ -1,47 +1,7 @@
-import pyvisa
-import time
+from arduino_device import ArduinoVISADevice, list_devices
 from random import *
 import matplotlib.pyplot as plt
 import csv
-
-def list_devices():
-    rm = pyvisa.ResourceManager("@py")
-
-    return rm.list_resources()
-
-class ArduinoVISADevice:
-
-    def __init__(self, port):
-        self.port = port
-        self.rm = pyvisa.ResourceManager("@py")
-        self.device = self.rm.open_resource(
-            "ASRL5::INSTR", read_termination="\r\n", write_termination="\n"
-        )
-
-    @staticmethod
-    def ADC_to_volt(ADC):
-        return round(int(ADC) * 3.3/1023, 2)
-
-    @staticmethod
-    def volt_to_ADC(U):
-        return int(round(U * 1023/3.3))
-
-    def get_identification(self):
-        return self.device.query("*IDN?")
-
-    def set_output_value(self, adc_value):
-        # adc_value % 1024 to get values between 0 and 1023
-        self.device.query(f"OUT:CH0 {adc_value % 1024}")
-        return
-
-    def get_output_value(self):
-        return self.device.query("OUT:CH0?")
-
-    def get_input_value(self, channel):
-        return int(self.device.query(f"MEAS:CH{channel}?"))
-    
-    def get_input_value_voltage(self, channel):
-        return self.ADC_to_volt(self.device.query(f"MEAS:CH{channel}?"))
 
 device = ArduinoVISADevice(port = "ASRL5::INSTR")
 
