f43b2d281c8e4a22a34f7222e21946cdf48d014c
M-Serrano-Altena <Marc.Serrano.Altena@gmail.com>
2023-11-06 19:44:31 +0100

    put the calculations in model and finished error

---
+++ diode_experiment.py
@@ -0,0 +1,46 @@
+# Marc Serrano Altena
+# 06-11-2023
+# gets the controller data to use for the experiment
+from arduino_device import ArduinoVISADevice, list_devices
+import numpy as np
+
+device = ArduinoVISADevice(port = "ASRL5::INSTR")
+
+class DiodeExperiment:
+    def __init__(self, device):
+        self.device = device
+        self.resistance = 220 #ohm
+        self.voltage_list = []
+        self.current_list = []
+        self.voltage_measurements = []
+        self.current_measurements = []
+    
+    def scan(self, start, stop):
+        for output_value in range(start, stop + 1): 
+            self.device.set_output_value(output_value)
+            voltage_tot = self.device.get_input_value_voltage(channel=1)
+            voltage_r = self.device.get_input_value_voltage(channel=2)
+            voltage_led = voltage_tot - voltage_r
+            current = voltage_r/self.resistance
+
+            self.voltage_list.append(voltage_led)
+            self.current_list.append(current)
+
+        # to turn of LED after experiment
+        self.device.set_output_value(0)
+        
+    def average_value_scan(self, start, stop):
+        for _ in range(0, 10):
+            self.scan(self, start, stop)
+            self.voltage_measurements.append(self.voltage_list)
+            self.current_measurements.append(self.current_list)
+        
+        # calculate the average value of 10 measurements for every output level
+        self.average_voltage_list = [np.average((volt1, volt2, volt3, volt4, volt5, volt6, volt7, volt8, volt9, volt10)) for volt1, volt2, volt3, volt4, volt5, volt6, volt7, volt8, volt9, volt10 in zip(self.voltage_measurements[0], self.voltage_measurements[1], self.voltage_measurements[2], self.voltage_measurements[3], self.voltage_measurements[4], self.voltage_measurements[5], self.voltage_measurements[6], self.voltage_measurements[7], self.voltage_measurements[8], self.voltage_measurements[9])]
+        self.average_current_list = [np.average((current1, current2, current3, current4, current5, current6, current7, current8, current9, current10)) for current1, current2, current3, current4, current5, current6, current7, current8, current9, current10 in zip(self.current_measurements[0], self.current_measurements[1], self.current_measurements[2], self.current_measurements[3], self.current_measurements[4], self.current_measurements[5], self.current_measurements[6], self.current_measurements[7], self.current_measurements[8], self.current_measurements[9])]
+        
+        # calculate the standard deviation (error margin) of the 10 measurements for every output level
+        self.error_voltage_list = [np.std((volt1, volt2, volt3, volt4, volt5, volt6, volt7, volt8, volt9, volt10)) for volt1, volt2, volt3, volt4, volt5, volt6, volt7, volt8, volt9, volt10 in zip(self.voltage_measurements[0], self.voltage_measurements[1], self.voltage_measurements[2], self.voltage_measurements[3], self.voltage_measurements[4], self.voltage_measurements[5], self.voltage_measurements[6], self.voltage_measurements[7], self.voltage_measurements[8], self.voltage_measurements[9])]
+        self.error_current_list = [np.std((current1, current2, current3, current4, current5, current6, current7, current8, current9, current10)) for current1, current2, current3, current4, current5, current6, current7, current8, current9, current10 in zip(self.current_measurements[0], self.current_measurements[1], self.current_measurements[2], self.current_measurements[3], self.current_measurements[4], self.current_measurements[5], self.current_measurements[6], self.current_measurements[7], self.current_measurements[8], self.current_measurements[9])]
+    
+
--- view.py
+++ view.py
@@ -2,34 +2,23 @@
 # 06-11-2023
 # views the data from the model experiment and makes a csv file and shows a I,U graph of the LED
 
-from diode_experimtent import DiodeExperiment, list_devices, device
-from random import *
+from diode_experiment import *
 import matplotlib.pyplot as plt
 import csv
-import numpy as np
 
 # U0 has the full current
 # U1 measures the current passing through the LED and resistor, which is the full current
 # U2 measures the current passing through the resistor
 
 def view_data(device):
-    voltage_scans = []
-    currents_scans = []
+    diode = DiodeExperiment(device)
+    diode.average_value_scan(start=0, stop=1023)
 
-    for _ in range(0, 10):
-        diode = DiodeExperiment(device)
-        diode.scan(start=0, stop=1023)
-        voltage_scans.append(diode.voltage_list)
-        currents_scans.append(diode.current_list)
-
-    print(voltage_scans)
-    average_voltage_list = [np.average(((volt1, volt2, volt3, volt4, volt5, volt6, volt7, volt8, volt9, volt10))) for volt1, volt2, volt3, volt4, volt5, volt6, volt7, volt8, volt9, volt10 in zip(voltage_scans[0], voltage_scans[1], voltage_scans[2], voltage_scans[3], voltage_scans[4], voltage_scans[5], voltage_scans[6], voltage_scans[7], voltage_scans[8], voltage_scans[9])]
-    average_current_list = [np.average((current1, current2, current3, current4, current5, current6, current7, current8, current9, current10)) for current1, current2, current3, current4, current5, current6, current7, current8, current9, current10 in zip(voltage_scans[0], voltage_scans[1], voltage_scans[2], voltage_scans[3], voltage_scans[4], voltage_scans[5], voltage_scans[6], voltage_scans[7], voltage_scans[8], voltage_scans[9])]
     # write the I,U data to a csv
     with open("metingen.csv", 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['U', 'I'])
-        for voltage, current in zip(diode.voltage_list, diode.current_list):
+        for voltage, current in zip(diode.average_voltage_list, diode.average_current_list):
             writer.writerow([voltage, current])    
 
     # plot (I, U) diagram of the LED
@@ -39,7 +28,7 @@ def view_data(device):
     plt.ylim(0, 0.0025)
     plt.xlabel("Voltage (V)")
     plt.ylabel("Current (A)")
-    plt.scatter(diode.voltage_list, diode.current_list, s=5, c='blue')
+    plt.errorbar(diode.average_voltage_list, diode.average_current_list, xerr=diode.error_voltage_list, yerr=diode.error_current_list, fmt='bo-', ecolor='k')
 
     plt.show()
     return
